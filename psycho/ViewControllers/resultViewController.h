e4b031e47d0d610e50870b87a386f91275d87619
gavrilak <lena@iMac-Misa.local>
2015-02-19 18:56:42 +0200

    232
    232323

---
+++ psycho/ViewControllers/resultViewController.h
@@ -0,0 +1,13 @@
+//
+//  resultViewController.h
+//  psycho
+//
+//  Created by Lena on 19.02.15.
+//  Copyright (c) 2015 bestappstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface resultViewController : UIViewController
+
+@end
