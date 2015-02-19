43f8e07d09452e7dd542b9368742115c6bd05edf
gavrilak <lena@iMac-Misa.local>
2015-02-19 13:28:12 +0200

    Initial Commit

---
+++ psycho/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  psycho
+//
+//  Created by Lena on 19.02.15.
+//  Copyright (c) 2015 bestappstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ psychoTests/psychoTests.m
@@ -0,0 +1,40 @@
+//
+//  psychoTests.m
+//  psychoTests
+//
+//  Created by Lena on 19.02.15.
+//  Copyright (c) 2015 bestappstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface psychoTests : XCTestCase
+
+@end
+
+@implementation psychoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
