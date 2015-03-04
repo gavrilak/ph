ce05902e676f5df4f9c9938eaa77b1661ced8c23
gavrilak <lena@iMac-Misa.local>
2015-03-04 13:03:46 +0200

    12
    12

--- psycho/ViewControllers/DSResultTestViewController.m
+++ psycho/ViewControllers/DSResultTestViewController.m
@@ -68,7 +68,20 @@
         dispatch_async(dispatch_get_main_queue(), ^{
             [activityView removeFromSuperview];
              [[UIApplication sharedApplication] endIgnoringInteractionEvents];
-            [self presentViewController:activityViewController animated:YES completion:nil];
+            
+            //if iPhone
+            if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+            {
+                [self presentViewController:activityViewController animated:YES completion:nil];
+            }
+            //if iPad
+            else
+            {
+                // Change Rect to position Popover
+                UIPopoverController *popup = [[UIPopoverController alloc] initWithContentViewController:activityViewController];
+                NSLog(@"%f",self.view.frame.size.width/2);
+                [popup presentPopoverFromRect:CGRectMake(self.view.frame.size.width/2, self.view.frame.size.height/4, 0, 0)inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+            }
             
         });
     });
